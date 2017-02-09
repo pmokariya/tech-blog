c88ba081c1bb52e61ebea1137ce7b3672a2b9555
pooja mokariya <poojalm747@gmail.com>
2017-02-09 16:00:45 +0530

    edit user function added

---
+++ db/migrate/20170209100115_add_fuild_to_user.rb
@@ -0,0 +1,8 @@
+class AddFuildToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :firstname, :string
+    add_column :users, :lastname, :string
+    add_column :users, :username, :string
+    add_column :users, :birthdate, :date
+  end
+end

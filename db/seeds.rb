e2512f08e7b24b4a542b9aecf0f921b9d3ba98f0
pooja mokariya <poojalm747@gmail.com>
2017-02-14 15:18:07 +0530

    user profile and user password change setup done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
- @user = User.create!(email: 'user@example.com', password: 'password', password_confirmation: 'password')
+@user = User.create!(username:"pooja",firstname:"pooja",lastname:"mokariya",email:'pooja@gmail.com', password: 'password', password_confirmation: 'password')
 5.times do
 	@post = Post.create!(title:"Ruby on rails" ,description:"Ruby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on railsRuby on rails",user_id:@user.id)
 	Comment.create!(text: "Nice i like it..!!",post_id:@post.id)
